c91e8f9a211447ce47a78e1846d9f8d76f78b952
Letícia Yamashita <miyuki.yamashita@engenharia.ufjf.br>
2024-01-22 18:16:09 -0300

    update exercicio 9

--- works/exercicio09.js
+++ works/exercicio09.js
@@ -16,74 +16,66 @@ material = setDefaultMaterial(); // create a basic material
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
 
+
 // Listen window size changes
-window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
 
 // Show axes (parameter is size of each axis)
-let axesHelper = new THREE.AxesHelper(12);
-scene.add(axesHelper);
+let axesHelper = new THREE.AxesHelper(15);
+scene.add( axesHelper );
 
 // create the ground plane
 let plane = createGroundPlaneXZ(30, 30)
 scene.add(plane);
 
-// create a sphere
-let esfera = new THREE.SphereGeometry(2, 17, 45);
-let material3 = new THREE.MeshBasicMaterial({ color: 0x9400d3 });
-let sphere1 = new THREE.Mesh(esfera, material3);
 
-sphere1.position.set(-5.0, 2.5, -6.0);
+  let sphereGeometry = new THREE.SphereGeometry(2, 17, 45);
+  let material3 = new THREE.MeshBasicMaterial({ color: 0x9400d3 });
+  let sphere = new THREE.Mesh(sphereGeometry, material3);
+  let sphere2 = new THREE.Mesh(sphereGeometry, material3);
+  sphere.position.set(-5,2.5,-5);
+  sphere2.position.set(-5.0,2.5,5);
+ 
+  scene.add(sphere);
+  scene.add(sphere2);
 
-scene.add(sphere1);
+ const lerpConfig = {
+  destination1: new THREE.Vector3(5, 2.5, -5),
+  destination2: new THREE.Vector3(5, 2.5, 5),
+  alpha1: 0.05,
+  alpha2: 0.02,
+  move1: false,
+  move2: false
+ }
 
-let esfera2 = new THREE.SphereGeometry(2, 17, 45);
-let material2 = new THREE.MeshBasicMaterial({ color: 0x9400d3 });
-let sphere2 = new THREE.Mesh(esfera2, material2);
+ const reset = () => {
+  lerpConfig.destination1 = {x: -5, y: 2.5, z: -5};
+  lerpConfig.destination2 = {x: -5, y: 2.5, z: 5};
+  
+ }
 
-sphere2.position.set(-5.0, 2.5, 6.0);
-
-scene.add(sphere2);
-
-const lerpConfig = {
-    destination: new THREE.Vector3(5, 2.5, -6),
-    destination2: new THREE.Vector3(5, 2.5, 6),
-    alpha: 0.05,
-    alpha2: 0.02,
-    move: false,
-    move2: false
-
-}
-
-const reset = () => {
-    lerpConfig.destination = {x: -5.0, y: 2.5, z: -6.0};
-    lerpConfig.destination2 = {x: -5.0, y: 2.5, z: 6.0};
-
-}
 
 document.getElementById('btnSphere1').addEventListener('click', () => {
-    lerpConfig.destination = {x: -5.0, y: 2.5, z: -6.0};
-    lerpConfig.move = true;
-
+  lerpConfig.destination1 = {x: 5, y: 2.5, z: -5};
+  lerpConfig.move1 = true;
 })
-
 document.getElementById('btnSphere2').addEventListener('click', () => {
-    lerpConfig.destination2 = {x: -5.0, y: 2.5, z: 6.0};
-    lerpConfig.move2 = true;
-
+  lerpConfig.destination2 = {x: 5, y: 2.5, z: 5};
+  lerpConfig.move2 = true;
 })
-
 document.getElementById('btnReset').addEventListener('click', () => {
-    reset();
+  
+  reset()
 
 })
 
 render();
-function render() {
+function render()
+{
 
-    lerpConfig.move ? sphere1.position.lerp(lerpConfig.destination, lerpConfig.alpha): 0;
-    lerpConfig.move2 ? sphere2.position.lerp(lerpConfig.destination2, lerpConfig.alpha2): 0;
-
-
-    requestAnimationFrame(render);
-    renderer.render(scene, camera) // Render scene
+  lerpConfig.move1 ? sphere.position.lerp(lerpConfig.destination1,lerpConfig.alpha1) : 0;
+  lerpConfig.move2 ? sphere2.position.lerp(lerpConfig.destination2,lerpConfig.alpha2) : 0;
+  
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
 }
