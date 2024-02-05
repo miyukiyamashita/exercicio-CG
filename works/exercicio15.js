f288a48b324429d4be9b923e8c646aa30a9fccd8
Letícia Yamashita <miyuki.yamashita@engenharia.ufjf.br>
2024-02-05 08:53:28 -0300

    Update exercicio15.js

--- works/exercicio15.js
+++ works/exercicio15.js
@@ -1,156 +1,102 @@
-/*
-Return a new CSG solid representing space in either this solid or in the
-solid `csg`. Neither this solid nor the solid `csg` are modified.
+import * as THREE from 'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {
+  initRenderer,
+  initCamera,
+  initDefaultBasicLight,
+  setDefaultMaterial,
+  onWindowResize,
+  createGroundPlaneXZ
+} from "../libs/util/util.js";
 
-    A.union(B)
+import { CSG } from '../libs/other/CSGMesh.js'
 
-    +-------+            +-------+
-    |       |            |       |
-    |   A   |            |       |
-    |    +--+----+   =   |       +----+
-    +----+--+    |       +----+       |
-         |   B   |            |       |
-         |       |            |       |
-         +-------+            +-------+
-
-Return a new CSG solid representing space in this solid but not in the
-solid `csg`. Neither this solid nor the solid `csg` are modified.
-
-    A.subtract(B)
-
-    +-------+            +-------+
-    |       |            |       |
-    |   A   |            |       |
-    |    +--+----+   =   |    +--+
-    +----+--+    |       +----+
-         |   B   |
-         |       |
-         +-------+
-
-Return a new CSG solid representing space both this solid and in the
-solid `csg`. Neither this solid nor the solid `csg` are modified.
-
-    A.intersect(B)
-
-    +-------+
-    |       |
-    |   A   |
-    |    +--+----+   =   +--+
-    +----+--+    |       +--+
-         |   B   |
-         |       |
-         +-------+
-*/
-
-import * as THREE from  'three';
-import Stats from '../build/jsm/libs/stats.module.js';
-import GUI from '../libs/util/dat.gui.module.js'
-import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
-import {initRenderer,
-        initCamera, 
-        initDefaultBasicLight,
-        createGroundPlane,
-        onWindowResize} from "../libs/util/util.js";
-
-import { CSG } from '../libs/other/CSGMesh.js'        
-import { TorusGeometry } from '../build/three.module.js';
-
-var scene = new THREE.Scene();    // Create main scene
-var stats = new Stats();          // To show FPS information
-
-var renderer = initRenderer();    // View function in util/utils
+let scene, renderer, camera, material, light, orbit;; // Initial variables
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // View function in util/utils
 renderer.setClearColor("rgb(30, 30, 40)");
-var camera = initCamera(new THREE.Vector3(4, -8, 8)); // Init camera in this position
-   camera.up.set( 0, 0, 1 );
+camera = initCamera(new THREE.Vector3(0, 20, 30)); // Init camera in this position
+material = setDefaultMaterial(); // create a basic material
+material.color.setHex(0x61b645); // set material color
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 
-window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
-initDefaultBasicLight(scene, true, new THREE.Vector3(12, -15, 20), 28, 1024) ;	
+// Listen window size changes
+window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
-var groundPlane = createGroundPlane(20, 20); // width and height (x, y)
-scene.add(groundPlane);
+
+let lightPosition = new THREE.Vector3(10, 1.9, 10);
+let spotLight = new THREE.SpotLight();
+setSpotLight(lightPosition, spotLight)
 
 // Show axes (parameter is size of each axis)
-var axesHelper = new THREE.AxesHelper( 12 );
-scene.add( axesHelper );
+let axesHelper = new THREE.AxesHelper(15);
+scene.add(axesHelper);
 
-var trackballControls = new TrackballControls( camera, renderer.domElement );
+// create the ground plane
+let plane = createGroundPlaneXZ(30, 30);
+scene.add(plane);
+let mesh;
 
-// To be used in the interface
-let mesh1, mesh2, mesh3;
-
-buildInterface();
 buildObjects();
 render();
 
-function buildObjects()
-{
-   let auxMat = new THREE.Matrix4();
-   
-   // Base objects
-   let cylinderMesh = new THREE.Mesh( new THREE.CylinderGeometry(1.5, 1.5, 6, 20))
-   let cylinder2Mesh = new THREE.Mesh( new THREE.CylinderGeometry(1, 0, 5.8, 20))
-   let torusMesh = new THREE.Mesh( new THREE.TorusGeometry(0.8, 0.1, 20, 20))   
+function buildObjects() {
+  let auxMat = new THREE.Matrix4();
 
-   // CSG holders
-   let csgObject, cylinder2CSG, cylinderCSG, torusCSG
+  let cylinderMesh2 = new THREE.Mesh(new THREE.CylinderGeometry(0.70, 0.70, 1.9, 20));
+  cylinderMesh2.position.set(0.0, 0.1, 0.0);
+  updateObject(cylinderMesh2);
+  let cylinderCSG2 = CSG.fromMesh(cylinderMesh2);
 
-   // Object 1 - Cylinder SUBTRACT Cylinder2
-   cylinderMesh.rotateZ(THREE.MathUtils.degToRad(180))
-   cylinder2Mesh.position.set(0.1, 0.1, 0.001)
-   updateObject(cylinder2Mesh) // update internal coords
-   cylinder2CSG = CSG.fromMesh(cylinder2Mesh)  
-   cylinderCSG = CSG.fromMesh(cylinderMesh)   
-   csgObject = cylinderCSG.subtract(cylinder2CSG) // Execute subtraction
-   mesh1 = CSG.toMesh(csgObject, auxMat)
-   mesh1.material = new THREE.MeshPhongMaterial({color: 'lightgreen'})
-   mesh1.position.set(0, 0, 1.5)
-   scene.add(mesh1)
+  let cylinderMesh = new THREE.Mesh(new THREE.CylinderGeometry(0.85, 0.85, 2, 20));
+  cylinderMesh.position.set(0.0, 0.0, 0.0);
+  updateObject(cylinderMesh);
+  let cylinderCSG = CSG.fromMesh(cylinderMesh);
+  cylinderCSG = cylinderCSG.subtract(cylinderCSG2);
 
+  let cubeMesh = new THREE.Mesh(new THREE.BoxGeometry(1.4, 2, 0.5));
+  cubeMesh.position.set(0, 0.1, 0);
+  updateObject(cubeMesh);
+  let cubeCSG = CSG.fromMesh(cubeMesh);
 
-   // Object 3 - Cylinder UNION torus
-   torusMesh.rotateX(THREE.MathUtils.degToRad(180))
-   torusMesh.position.set(1.3, 0, 0.5) // reset position
-   updateObject(torusMesh) 
-   torusCSG = CSG.fromMesh(torusMesh)
-   cylinder2CSG = CSG.fromMesh(cylinder2Mesh)
+  let torusMesh = new THREE.Mesh(new THREE.TorusGeometry(0.75, 0.15, 20, 20));
+  torusMesh.position.set(0.6, 0.0, 0.0);
+  updateObject(torusMesh);
+  let torusCSG = CSG.fromMesh(torusMesh);
+  torusCSG = torusCSG.subtract(cubeCSG);
 
-   csgObject = cylinder2CSG.union(torusCSG) // Execute union
-   mesh3 = CSG.toMesh(csgObject, auxMat)
-   mesh3.material = new THREE.MeshPhongMaterial({color: 'lighthreen'})
-   mesh3.position.set(0, 0, 1.5)
-   scene.add(mesh3)
+  let csgObject = cylinderCSG.union(torusCSG);
+
+  mesh = CSG.toMesh(csgObject, auxMat);
+  mesh.material = new THREE.MeshPhongMaterial({ color: 'lightblue' });
+  mesh.position.set(0, 1, 0);
+  scene.add(mesh);
+
 }
 
-function updateObject(mesh)
-{
-   mesh.matrixAutoUpdate = false;
-   mesh.updateMatrix();
+function setSpotLight(position, spot) {
+  spot.position.copy(position);
+  spot.angle = THREE.MathUtils.degToRad(40);
+  spot.decay = 2; // The amount the light dims along the distance of the light.
+  spot.penumbra = 1; // Percent of the spotlight cone that is attenuated due to penumbra. 
+
+  // Shadow settings
+  spot.castShadow = true;
+  spot.shadow.mapSize.width = 512;
+  spot.shadow.mapSize.height = 512;
+  spot.intensity = 0.2
+
+  scene.add(spot);
 }
 
-function buildInterface()
-{
-  var controls = new function ()
-  {
-    this.wire = false;
-    
-    this.onWireframeMode = function(){
-       mesh1.material.wireframe = this.wire;
-       mesh2.material.wireframe = this.wire;       
-       mesh3.material.wireframe = this.wire;              
-    };
-  };
-
-  // GUI interface
-  var gui = new GUI();
-  gui.add(controls, 'wire', false)
-    .name("Wireframe")
-    .onChange(function(e) { controls.onWireframeMode() });
+function updateObject(mesh) {
+  mesh.matrixAutoUpdate = false;
+  mesh.updateMatrix();
 }
 
-function render()
-{
-  stats.update(); // Update FPS
-  trackballControls.update();
+
+function render() {
   requestAnimationFrame(render); // Show events
   renderer.render(scene, camera) // Render scene
 }
